164d6bc67a3947b35b3902754dbd4df22882ea85
unknown <elijahsatten@hotmail.com>
2023-10-05 21:12:01 -0400

    added variables and made a function for generating parameters

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,39 @@
 // This is just a code for a place to start
-var genBtn = $('#genBtn');
+var genButton = $("#genBtn");
 var checkedEl = $('input:checked');
+var tiles = $('#parameter1');
+var tileSize = $('#parameter2');
+var r = $('#parameter3');
+var g = $('#parameter4');
+var b = $('#parameter5');
 
 function getApi() {
-    var backgroundApi = `https://php-noise.com/noise.php?r=${r}&g=${g}&b=${b}&tiles=${tiles}&tileSize=${tileSize}&borderWidth=${borderWidth}&mode=${mode}&json`;
-    var emojiApi = 'https://emojihub.yurace.pro/api/random/category/smileys-and-people'
+  var backgroundApi = `https://php-noise.com/noise.php?r=${r}&g=${g}&b=${b}&tiles=${tiles}&tileSize=${tileSize}&json`;
+  var emojiApi = 'https://emojihub.yurace.pro/api/random/category/smileys-and-people';
   
-    fetch(backgroundApi)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          console.log("Test")
-        }
-      });
+  fetch(backgroundApi)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      console.log("Test");
+    }
+  });
 
-      fetch(emojiApi)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          console.log("Test2")
-        }
-      });
-  }
+  fetch(emojiApi)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      console.log("Test2");
+    }
+  });
+  generateParameters();
+}
+
+function generateParameters() {
   
-  genBtn.on('click', getApi);
+}
+genButton.on('click', getApi);
