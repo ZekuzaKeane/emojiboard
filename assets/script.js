25c4aabda6130795f63c9454efe00467ea55b10a
Ryan Caltabiano <rcaltabiano90@gmail.com>
2023-10-11 20:43:08 -0400

    JS and CSS edits

--- assets/script.js
+++ assets/script.js
@@ -27,6 +27,7 @@ function getApi(event) {
     return response.json();
   })
   .then(function (data) {
+    localStorage.setItem('backgroundData', JSON.stringify(data));
     var imageResults = document.getElementById('imageResults');
     imageResults.src = data.uri;
   })
@@ -38,8 +39,8 @@ function getApi(event) {
 };
 
 function getEmojiApi() {
+
   var emojiApi = 'https://emojihub.yurace.pro/api/random/category/smileys-and-people'
-  
   fetch(emojiApi)
     .then(function (response) {
     console.log(response.status)
@@ -47,10 +48,31 @@ function getEmojiApi() {
     return response.json();
   })
     .then(function(data) {
+      localStorage.setItem('emojiData', JSON.stringify(data));
       var emojiResults = document.getElementById('emoji');
       emojiResults.innerHTML = data.htmlCode.join('');
   })
   .catch(function(error) {
       console.error('Error:', error);
   });
+
+   displayStorage();
   }
+
+function displayStorage(event) {
+    var backgroundData = JSON.parse(localStorage.getItem('backgroundData'));
+    var emojiData = JSON.parse(localStorage.getItem('emojiData'));
+    
+    if (backgroundData && emojiData) {
+      $('#history').empty()
+      var overFlow = 0
+      var storedData = $('<p>').text("Background: " + backgroundData.uri);
+      var storedData2 = $('<p>').text("Emoji Unicode: " + emojiData.unicode);
+      $('#history').append(storedData);
+      $('#history').append(storedData2);
+    }
+
+    if (overFlow >= 2){
+      $('#history').innerHTML
+    }
+  };
